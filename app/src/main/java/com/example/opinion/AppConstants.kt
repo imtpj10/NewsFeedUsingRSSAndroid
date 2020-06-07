c1f855e9252288b09463887b932b2106e4d31ca4
Tulsi <tpj10@iitbbs.ac.in>
2020-06-07 22:26:13 +0530

    Added fragments for each newspaper

---
+++ app/src/main/java/com/example/opinion/AppConstants.kt
@@ -0,0 +1,10 @@
+package com.example.opinion
+
+
+object Constants {
+    const val THE_HINDU = "https://www.thehindu.com/news/feeder/default.rss"
+    const val THE_TIMES_OF_INDIA = "https://timesofindia.indiatimes.com/rssfeedstopstories.cms"
+    const val LIVEMINT = "https://www.livemint.com/rss/news"
+    const val THE_INDIAN_EXPRESS = "https://indianexpress.com/section/india/feed/"
+    const val ECONOMIST = "https://www.economist.com/the-world-this-week/rss.xml"
+}
--- app/src/main/java/com/example/opinion/MyItemRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/opinion/MyItemRecyclerViewAdapter.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide.init
+import org.jsoup.Connection
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
 import org.jsoup.nodes.Document
@@ -41,7 +42,7 @@ class MyItemRecyclerViewAdapter(
         val item = mValues[position]
         holder.titleTV?.text = item.title
         holder.pubDateTV?.text = item.pubDate
-        var link = getFeaturedImageLink(item.description)
+        var link = getFeaturedImageLink(item.link)
         if (link != null) {
             context?.let {
                 GlideApp.with(it)
@@ -69,22 +70,43 @@ class MyItemRecyclerViewAdapter(
     }
 
 
-    private fun getFeaturedImageLink(htmlText: String): String? {
+    private fun getFeaturedImageLink( link: String): String? {
+
         var result: String? = null
 
         val stringBuilder = StringBuilder()
-        try {
-            val doc: Document = Jsoup.parse(htmlText)
-            val imgs: Elements = doc.select("img")
-            for (img in imgs) {
-                var src = img.attr("src")
-                result = src
+       // try {
+//            val conn:Connection = Jsoup.connect(htmlText).userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
+//            val doc: Document = conn.get()
+//            val imgs: Elements = doc.select("img")
+//            for (img in imgs) {
+//                var src = img.attr("src")
+//                result = src
+            val src : String
+        if(link[8] == 't') {
+            src= "https://i.postimg.cc/tJVmSG7Y/download-2.jpg"
 
+        } else if(link[8] =='i') {
+            src ="https://i.postimg.cc/pdLv0yvr/download.png"
+        }else {
+            if(link[12]=='t') {
+                src = "https://i.postimg.cc/BbsMgPWw/The-Hindu-Logo-3.png"
+            }
+            else if( link[12] =='l') {
+
+                src = "https://i.postimg.cc/y6MVVVmp/default-621.jpg"
+            }else {
+                src = "https://i.postimg.cc/tgW3MRz0/download-2.png"
             }
 
-        } catch (e: IOException) {
-
         }
+        result = src
+
+      //  }
+
+       // } catch (e: IOException) {
+
+     //   }
         return result
 
     }
